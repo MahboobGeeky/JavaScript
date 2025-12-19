3775cf9c46c109034800977f7bb316f172a3e99d
MahboobGeeky <mahboob9184@gmail.com>
2025-12-19 17:11:43 +0530

    Done

---
+++ 3. Operators/operator.js
@@ -0,0 +1,17 @@
+// Arithmatic, comparison, logical, assignment, unary, ternary
+// 1. Arithmatic -> +,-,/,*,%,**
+// 1 + 2 = 3
+// "al" + "am" = "alam" -> concatination
+
+// 2 ** 3 = 8; -> power 
+
+// comparison->
+// = -> assingment operator
+// == -> camparison(loose)
+// === -> comparison(strict)
+// !=
+// !==
+// >=
+// <=
+// <
+// >
