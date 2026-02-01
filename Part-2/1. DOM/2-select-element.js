d9eb277fe16875001b0aaf531716506ac763d1f3
MahboobGeeky <mahboob9184@gmail.com>
2026-02-01 21:10:07 +0530

    Done

--- Part-2/1. DOM/2-select-element.js
+++ Part-2/1. DOM/2-select-element.js
@@ -1,2 +1,9 @@
 let h1 = document.querySelector("h1");
 h1.innerText = "sab badhiya hai.";
+
+// create element
+// append/prepend kro, jaha bhi element chahiye waha
+let h2 = document.createElement("h2");
+h2.textContent = "hello ji kaise ho";
+document.querySelector("body").prepend(h2);
+ 
