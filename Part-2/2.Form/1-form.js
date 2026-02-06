c45af06606f656925cc9ccdfe2ba29f6ec289fb8
MahboobGeeky <mahboob9184@gmail.com>
2026-02-07 00:40:58 +0530

    Done

---
+++ Part-2/2.Form/1-form.js
@@ -0,0 +1,11 @@
+let nm = document.querySelector("#name");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(dets){
+    dets.preventDefault(); 
+    if(nm.value.length <=2 ){
+        document.querySelector("small").style.display = "initial";
+    }else{
+        document.querySelector("small").style.display ="none";
+    }
+});
---
+++ Part-2/2.Form/Email-validator/script.js
@@ -0,0 +1,35 @@
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (dets) {
+  dets.preventDefault();
+
+  document.querySelector("#emailError").textContent = "";
+  document.querySelector("#passwordError").textContent = "";
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+  let emailAns = emailRegex.test(email.value);
+  let passwordAns = passwordRegex.test(password.value); // true
+
+  let isValid = true;
+
+  if (!emailAns) {
+    document.querySelector("#emailError").textContent = "Email is incorrect";
+    document.querySelector("#emailError").style.display = "initial";
+    isValid = false;
+  }
+
+  if (!passwordAns) {
+    document.querySelector("#passwordError").textContent =
+      "Password is incorrect";
+    document.querySelector("#passwordError").style.display = "initial";
+    isValid = false;
+  }
+  
+  if(isValid){
+    document.querySelector("#resultMessage").textContent = "Everything is correct";
+  }
+});
