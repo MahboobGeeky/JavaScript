43e509d1bcb12d3dd7347369b8c6f49f626d73cf
MahboobGeeky <mahboob9184@gmail.com>
2026-02-12 00:46:54 +0530

    Done

--- Part-2/4. LocalStorage, SessionStrorage, and Cookies/4-Theme Preference/script.js
+++ Part-2/4. LocalStorage, SessionStrorage, and Cookies/4-Theme Preference/script.js
@@ -1,5 +1,17 @@
 
  // true->os theme is dark
+function setDarkOrLight() {
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches){
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+        
+    }else{
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+};
+
+document.body.classList.add(localStorage.getItem("theme"));
 
 let btn = document.querySelector("button");
 
@@ -15,16 +27,6 @@ btn.addEventListener("click", function(){
     }
 });
 
-function setDarkOrLight() {
-    if(window.matchMedia("(prefers-color-scheme: dark)").matches){
-        document.body.classList.add("dark");
-        document.body.classList.remove("light");
-        
-    }else{
-        document.body.classList.add("light");
-        document.body.classList.remove("dark");
-    }
-};
 
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function(){
     setDarkOrLight();
--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let inp = document.querySelector("input");
-// inp.addEventListener("input", function (data) {
-//     console.log(data);
-// });
+// let inp = document.querySelector("input");
+// // inp.addEventListener("input", function (data) {
+// //     console.log(data);
+// // });
 
 
-inp.addEventListener("input", function(details){
-    console.log(details.data);
-})
+// inp.addEventListener("input", function(details){
+//     console.log(details.data);
+// })
+
+
+
+
+let user = {
+  name: "anisha",
+  greet: function () {
+    console.log("Hello, " + this.name);
+  }
+};
+
+user.greet(); // Hello, Amisha
+
+user.name = "Mahboob";
+
+user.greet(); // Hello, Mahboob
+
+
+
