19bcdbaa2865947aab1340036ca5f67c92554fec
MahboobGeeky <mahboob9184@gmail.com>
2026-02-15 22:14:42 +0530

    Done

---
+++ Part-3/7-OOPS/script.js
@@ -0,0 +1,18 @@
+// OOPS
+
+function CreatePencil(name, price, color, company) {
+  this.name = name;
+  this.price = price;
+  this.color = color;
+  this.company = company;
+  this.write = function (text) {
+    let h1 = document.createElement("h1");
+    h1.textContent = text;
+    h1.style.color = color;
+    document.body.append(h1);
+  };
+}
+
+let pencil1 = new CreatePencil("Natraj", 10, "red", "natraj");
+let pencil2 = new CreatePencil("Doms", 10, "green", "doms");
+
