327cf306198dbd25224c9750d6ad9bba7546bcf9
MahboobGeeky <mahboob9184@gmail.com>
2026-02-18 01:41:00 +0530

    Done

--- Part-4/2-Throttle.js
+++ Part-4/2-Throttle.js
@@ -10,3 +10,4 @@ function throttle(func, limit) {
     }
   };
 }
+
---
+++ Part-4/4-import-module/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", async function () {
+  let heavy = await import("./heavy.js");
+  heavy.veryHeavy();
+});
